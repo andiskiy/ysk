49ad333b3accc61c4e0e19d8d34208e459483cc1
Magomed Saipudinov <saipudinov.magomed@gmail.com>
2023-02-22 02:15:38 +0300

    Speech recognition and sending text to telegram

--- app/services/telegram/audio/recognize_service.rb
+++ app/services/telegram/audio/recognize_service.rb
@@ -3,12 +3,14 @@
 module Telegram
   module Audio
     class RecognizeService < ApplicationService
+      SUCCESS_CODE = 200
+
       attr_accessor :chat_id, :file_id, :message_id
 
       def process
         validate
 
-        download_audio
+        send_message_to_tg
       end
 
       private
@@ -17,10 +19,30 @@ module Telegram
         %w[chat_id message_id file_id].each do |key|
           halt(key.to_sym, :blank) if send(key).blank?
         end
+
+        errors.add(:audio_file, :not_found) if audio_file.response_code != SUCCESS_CODE
       end
 
-      def download_audio
-        ::Api::Telegram::DownloadFile.call(file_path: file_path)
+      def send_message_to_tg
+        Telegram.bot.send_message(chat_id: chat_id, text: generate_tg_message, reply_to_message_id: message_id)
+      end
+
+      def yandex_api
+        @yandex_api ||= ::Api::Yandex::SpeechKit.call(file: audio_file.response_body)
+      end
+
+      def generate_tg_message
+        return I18n.t('telegram.errors.invalid_audio_file') if failed?
+
+        if yandex_api.success?
+          yandex_api.response_body['result']
+        else
+          I18n.t('telegram.errors.failed_recognize')
+        end
+      end
+
+      def audio_file
+        @audio_file ||= ::Api::Telegram::DownloadFile.call(file_path: file_path)
       end
 
       def file_path
--- spec/services/telegram/audio/recognize_service_spec.rb
+++ spec/services/telegram/audio/recognize_service_spec.rb
@@ -16,6 +16,8 @@ RSpec.describe Telegram::Audio::RecognizeService do
   let(:file_id) { Faker::Lorem.word }
   let(:message_id) { Faker::Number.number }
   let(:file_path) { 'voice/file_2.oga' }
+  let(:text) { 'result' }
+
   let(:get_file_response) do
     {
       'ok' => true,
@@ -27,12 +29,92 @@ RSpec.describe Telegram::Audio::RecognizeService do
     }
   end
 
+  let(:send_message_params) do
+    {
+      chat_id: chat_id,
+      text: text,
+      reply_to_message_id: message_id
+    }
+  end
+
+  let(:stub_download_file) do
+    allow(::Api::Telegram::DownloadFile).to(
+      receive(:call)
+        .with(file_path: file_path)
+        .and_return(Struct.new(:response_body, :response_code).new('response_body', d_f_response_code)),
+    )
+  end
+
+  let(:stub_speech_kit) do
+    allow(::Api::Yandex::SpeechKit).to(
+      receive(:call)
+        .with(file: 'response_body')
+        .and_return(
+          Struct
+            .new(:success?, :response_body)
+            .new(speech_kit_success, { 'result' => text }),
+        ),
+    )
+  end
+
+  let(:stub_send_message) { allow(Telegram.bot).to receive(:send_message).with(send_message_params) }
+
+  let(:d_f_response_code) { 200 }
+  let(:speech_kit_success) { true }
+
   before do
-    allow(::Api::Telegram::DownloadFile).to receive(:call).with(file_path: file_path)
     allow(Telegram.bot).to receive(:get_file).with(file_id: file_id).and_return(get_file_response)
   end
 
-  it { expect(call_service).to be_success }
+  context 'when everything is ok' do
+    before do
+      stub_download_file
+      stub_speech_kit
+      stub_send_message
+    end
+
+    it 'is success' do
+      expect(call_service).to be_success
+      expect(Telegram.bot).to have_received(:send_message).with(send_message_params)
+    end
+  end
+
+  context 'when audio file not found' do
+    before do
+      stub_download_file
+      stub_send_message
+    end
+
+    let(:text) { I18n.t('telegram.errors.invalid_audio_file') }
+    let(:d_f_response_code) { 401 }
+
+    it 'is failed' do
+      expect(call_service).to be_failed
+      expect(Telegram.bot).to have_received(:send_message).with(send_message_params)
+    end
+  end
+
+  context 'when get_file is failed' do
+    let(:get_file_response) { { 'ok' => false } }
+
+    it { expect(call_service).to be_failed }
+  end
+
+  context 'when yandex speech api failed' do
+    before do
+      stub_download_file
+      stub_speech_kit
+      stub_send_message
+    end
+
+    let(:text) { I18n.t('telegram.errors.failed_recognize') }
+    let(:speech_kit_success) { false }
+
+    it 'is success' do
+      expect(call_service).to be_success
+      expect(Telegram.bot).to have_received(:send_message).with(send_message_params)
+    end
+  end
 
   context 'when chat_id is nil' do
     let(:chat_id) { nil }
